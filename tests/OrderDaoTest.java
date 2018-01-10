7973827f2f873b4d3056affd71a3e6776d56040b
ZsuzsaPetho <zsuzsapethocodecool@gmail.com>
2018-01-10 13:37:05 +0100

    OrderDaoTestMem all method tested and working

--- tests/OrderDaoTest.java
+++ tests/OrderDaoTest.java
@@ -1,8 +1,8 @@
 import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.OrderDaoJDBC;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.model.Order;
-import com.codecool.shop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,19 +18,49 @@ public abstract class OrderDaoTest<T extends OrderDao> {
 
     protected abstract void setUpInstance();
 
+    private Order getSampleOrder() {
+        CartDaoMem cartDaoMem = CartDaoMem.getInstance();
+
+        List<String> list = new ArrayList<>(Arrays.asList("Name", "E-mail", "Phone Number", "Billing Address", "Billing City", "Billing Zipcode", "Billing Country","Shipping Address", "Shipping City", "Shipping Zipcode",  "Shipping Country"));
+        List<String> data = new ArrayList<>(Arrays.asList("Gipsz Jakab", "testemail@gmail.com", "303377027", "Kőbányai utca", "Budakalász", "2011", "Hungary","Déryné utca", "Gödöllő", "2100",  "Hungary"));
+        LinkedHashMap userData = new LinkedHashMap();
+        for (int i=0; i<list.size(); i++){
+            userData.put(list.get(i), data.get(i));
+        };
+        return new Order(cartDaoMem.getAll(), userData);
+    }
+
     @BeforeEach
     public void setUp() {
         setUpInstance();
+        TestDataMem.fillInstances();
     }
 
     @Test
     public void testEmptyOrderList() {
+        TestDataMem.clearInstances();
         ArrayList<Order> expected = new ArrayList <>();
         assertTrue(instance.getAll().equals(expected));
     }
 
     @Test
-    public void testAddMethod() {
+    public void testAddMethodAddedOneOrder() {
+        int size = instance.getAll().size();
+        Order added = getSampleOrder();
+        instance.add(added);
+        assertTrue(instance.getAll().size() == size+1);
+    }
+
+    @Test
+    public void testGetAllMethod() {
+        assertTrue(instance.getAll().size() == 1);
+    }
+
+    @Test
+    public void testGetLastMethod() {
+        Order expected = getSampleOrder();
+        expected.setId(1);
+        assertEquals(instance.getLast(), expected);
     }
 
 }
@@ -57,7 +87,7 @@ class OrderDaoJDBCTest extends OrderDaoTest<OrderDaoJDBC> {
 
     @Override
     protected void setUpInstance() {
-
+        instance = createInstance();
     }
 
 }
