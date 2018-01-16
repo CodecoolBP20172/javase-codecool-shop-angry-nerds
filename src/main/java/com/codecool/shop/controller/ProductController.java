271ae0fe5fece535ab6c75ea5a43f9c15c927d8a
mateszummer <szummerm@gmail.com>
2018-01-16 13:49:36 +0100

    ProductCategory - logback finished

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -32,26 +32,31 @@ public class ProductController {
         params.put("suppliers", supplierDataStore.getAll());
         params.put("products", productDataStore.getAll());
         params.put("cartSize", cartData.getCount());
-        logger.warn("Rendering index page with params:{}",params);
+        logger.debug("Rendering index page with params:{}",params);
         return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderProductsBy(String supOrCat, String id) {
+        logger.debug("Args: supOrCat: {}, id: {}",supOrCat,id);
         Map params = new HashMap<>();
         params.put("categories", productCategoryDataStore.getAll());
         params.put("suppliers", supplierDataStore.getAll());
         params.put("cartSize", cartData.getCount());
         if (supOrCat.equals("supplier")){
             Supplier supplier = supplierDataStore.find(Integer.parseInt(id));
+            logger.warn("Find supplier by id {} = {}", id, supplier);
             params.put("title", supOrCat + " : " +  supplier.getName());
             params.put("products", productDataStore.getBy(supplier));
         } else if ( supOrCat.equals("category")) {
             ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(id));
+            logger.warn("Find category by id {} = {}", id, productCategory);
             params.put("title", supOrCat + " : " +  productCategory.getName());
             params.put("products", productDataStore.getBy(productCategory));
         } else {
+            logger.error("SupOrCat is invalid: {} ", supOrCat);
             return new ModelAndView(params, "404");
         }
+        logger.debug("Rendering index page with params: {} ", params);
         return new ModelAndView(params, "product/index");
     }
 
@@ -59,6 +64,7 @@ public class ProductController {
     public static ModelAndView forms(String title) {
         Map params = new HashMap<>();
         params.put("title", title);
+        logger.debug("Rendering forms page with params: {}", params);
         return new ModelAndView(params, "forms");
     }
 
@@ -66,6 +72,7 @@ public class ProductController {
         Map params = new HashMap<>();
         params.put("title", title);
         params.put("method", method);
+        logger.debug("Rendering forms page with params: {}", params);
         return new ModelAndView(params, "forms");
     }
 
@@ -76,11 +83,10 @@ public class ProductController {
         params.put("categories", productCategoryDataStore.getAll());
         params.put("suppliers", supplierDataStore.getAll());
         Product product = productDataStore.find(Integer.parseInt(id));
+        logger.warn("Find product by id {} = {}", id, product);
         cartData.add(product);
         params.put("cartSize", cartData.getCount());
-        logger.warn("why u no work");
-
-
+        logger.debug("Rendering index page with params: {}", params);
         return new ModelAndView(params, "product/index");
     }
 
@@ -94,6 +100,7 @@ public class ProductController {
         params.put("cartProducts", cartData.getAll());
         params.put("cartSize", cartData.getCount());
         int firstLoop = 0;
+        logger.info("Checking if currencies are the same in cart ...");
         for (Map.Entry<Product, Integer> entry : cartData.getAll().entrySet()) {
             sumPrice += entry.getKey().getDefaultPrice()*entry.getValue();
             if (firstLoop++ == 0) {
@@ -102,15 +109,18 @@ public class ProductController {
                 nextCurrency = entry.getKey().getDefaultCurrency().toString();
                 if (!firstCurrency.equals(nextCurrency)) {
                     isError = true;
+                    logger.error("Found 2 different currencies in cart: {} != {}", firstCurrency, nextCurrency);
                     params.put("errorMessage", "Oh no! You cant order items with different currencies.");
                 }
             }
-
-
+        }
+        if (isError == false) {
+            logger.info("Currency check passed, no anomaly detected.");
         }
         params.put("isError", isError);
         params.put("sumPrice", sumPrice);
         params.put("currency", firstCurrency);
+        logger.debug("Rendering cart with params: {}", params);
         return new ModelAndView(params, "product/cart");
 
     }
@@ -126,16 +136,19 @@ public class ProductController {
             json.put(data, req.queryParams(data));
         }
         Order order = new Order(cartData.getAll(), userData);
+        logger.debug("New order: {}", order);
         orderData.add(order);
         json.put("Ordered Items", cartData.getAll());
         json.put("Order ID", orderData.getLast().getId());
-
+        logger.info("Writing order to file...");
         try (FileWriter file = new FileWriter("target/json/order" + orderData.getLast().getId() + ".json")) {
 
             file.write(json.toJSONString());
             file.flush();
+            logger.info("Order's successfully written to a file.");
 
         } catch (IOException e) {
+            logger.error("Couldn't write order to a file.");
             e.printStackTrace();
         }
     }
@@ -146,24 +159,30 @@ public class ProductController {
         for (Map.Entry<Product, Integer> entry : cartData.getAll().entrySet()) {
             sumPrice += entry.getKey().getDefaultPrice() * entry.getValue();
         }
+        logger.error("CLEAR CART IS NOT WORKING ATM");
         cartData.clearCart();
         params.put("message", "Payment successful!");
         params.put("userData", orderData.getLast().getUserData());
         params.put("orderData", orderData.getLast().getOrder());
         params.put("orderId", orderData.getLast().getId());
         params.put("sumPrice", sumPrice);
+        logger.info("Sending email with order...");
+        logger.debug("Sending email with order: {}", orderData.getLast());
         Email.sendEmail(orderData.getLast());
         return new ModelAndView(params, "confirmation");
     }
 
     public static void removeProduct(Integer id) {
+        logger.info("Removing product with id: {}", id);
         cartData.remove(id);
     }
 
     public static void changeQuantity(Integer id, Integer quantity){
         if (quantity == 0) {
+            logger.info("Quantity = {}. Removing product with id: {}",quantity, id);
             cartData.remove(id);
         } else {
+            logger.info("Quantity = {}. Setting quantity of the product with id: {}",quantity, id);
             cartData.setQuantity(id, quantity);
         }
 
